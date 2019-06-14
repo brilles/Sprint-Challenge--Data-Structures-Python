fe26217609520f2147e351a3abb5637ebca8e222
Brian Illes <brianrilles@gmail.com>
2019-06-14 10:09:13 -0700

    Stretch complete

--- names/names.py
+++ names/names.py
@@ -3,16 +3,21 @@ import time
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+names_1 = set(f.read().split("\n"))  # List containing 10000 names
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+names_2 = set(f.read().split("\n"))  # List containing 10000 names
 f.close()
 
-duplicates = set(names_1) & set(names_2)
+# duplicates = names_1 & names_2
+duplicates=[]
+
+#stretch 
+# for i in names_1:
+#     if i in names_2:
+#         duplicates.append(i)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
